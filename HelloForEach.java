9c423803f706779e2533c470c7f84f495a3fb7a5
Riddhima Singh <85569566+zany1105@users.noreply.github.com>
2026-04-13 09:52:02 +0530

    Update HelloForEach.java

---
+++ HelloForEach.java
@@ -0,0 +1,11 @@
+public class HelloForEach {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        } else {
+            for (String name : args) {
+                System.out.println("Hello, " + name + "!");
+            }
+        }
+    }
+}
